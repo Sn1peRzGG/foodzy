23c50e353b9e1c8c258b253efed4a9a0d07429e2
”Sn1peRzGG” <afly367@gmail.com>
2026-06-08 09:56:37 +0300

    bug fixes

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -79,7 +79,7 @@ const Banner = () => {
 						</a>
 					</span>
 					<Link
-						href={'http://localhost:3000/'}
+						href={'/'}
 						className='font-normal text-[11px] lg:text-[13px] text-[#FFF5DB] hover:underline'
 					>
 						foodzy.com
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'
+
 const api = axios.create({
-	baseURL: 'http://localhost:5555/api/v1',
+	baseURL: API_URL,
 	withCredentials: true,
 	headers: {
 		'Content-Type': 'application/json',

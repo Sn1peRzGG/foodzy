70bf7fbe4c9fa038074607e88d68ef30c0125b4e
”Sn1peRzGG” <afly367@gmail.com>
2026-06-08 10:07:01 +0300

    bug fixes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5555'
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5555'
+
+const API_URL = `${BASE_URL}/api/v1`
 
 const api = axios.create({
 	baseURL: API_URL,
